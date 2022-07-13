2082bfebed882231fc79279590ee298413ab7e82
Prateek Sharma <prateekshrmprateek@gmail.com>
2022-07-13 23:22:04 +0530

    more patterns

--- Pattern/src/Pattern.java
+++ Pattern/src/Pattern.java
@@ -2,7 +2,7 @@
 public class Pattern {
 	public static void main(String args[])
 	{
-		pattern1(5);
+		pattern2(5);
 	}
 	static void pattern1(int n) {
 //		****
@@ -19,4 +19,23 @@ public class Pattern {
 			System.out.println();
 		}
 	}
+	static void pattern2(int n) {
+//		*       1 - 1
+//		**		2 - 2
+//		***     3  - 3
+//		****	4  - 4
+//		***		5  - 3
+//		**		6  - 2
+//		*		7  - 1
+		for(int row = 1; row <= 2*n -1 ; row++)	
+		{
+			int noOfCols = row > n ? 2*n - row : row;
+			for (int col = 1; col <= noOfCols; col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
 }
